46f5d676621bf1f2e29e31deb43fa85d5ccc4cbe
Bruno <amorimbq@gmail.com>
2021-11-29 22:24:47 -0300

    config: had to add a line for the windows standard localhost ip address

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import connexion
 from flask import render_template
+
 import config
 
 app = config.connex_app
@@ -12,4 +13,5 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='127.0.0.1', port=5000, debug=True) # Para Windows
+    # app.run(host='0.0.0.0', port=5000, debug=True) # Para Linux/Mac
